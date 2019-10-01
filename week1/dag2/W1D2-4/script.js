19b0a51d6ea2f95b2fd6c95ca18efea8742b4fa5
Doedels <sandermarkies@yahoo.com>
2019-10-01 15:55:03 +0200

    Toevoegen opdracht W1D2-4

---
+++ week1/dag2/W1D2-4/script.js
@@ -0,0 +1,23 @@
+const age = 46;
+
+if (age >= 18) {
+  console.log("Welkom, kom binnen.");
+} else {
+  console.log("U bent te jong en mag helaas niet naar binnen.");
+}
+
+const isFemale = false;
+
+if (isFemale) {
+  console.log("Welkom, kom binnen.");
+} else {
+  console.log("Vanavond is Ladies Night, morgen bent u weer welkom.");
+}
+
+const driverStatus = "Bob";
+
+if (driverStatus.toLowerCase() === "bob") {
+  console.log("goede reis, rij voorzichtig.");
+} else {
+  console.log("U mag niet rijden, vraag of u met Bob mee kunt rijden.");
+}
