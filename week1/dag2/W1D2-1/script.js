e2bb4199b6aef53dc7ee63b90131183bb15ae9e4
Doedels <sandermarkies@yahoo.com>
2019-10-01 10:43:44 +0200

    Toevoegen opdracht W1D2-1

---
+++ week1/dag2/W1D2-1/script.js
@@ -0,0 +1,16 @@
+console.log("Hello Winc Academy");
+let naam = "Sander";
+console.log(naam);
+//een variable kan niet de naam 'if' krijgen, omdat dat een 'reserved keyword' is
+let eersteNummer = 2;
+let tweedeNummer = 3;
+console.log(eersteNummer + tweedeNummer);
+console.log(3 + 4);
+console.log(13 - 2);
+console.log(3 * 3);
+console.log(10 / 3);
+console.log(10 % 3);
+let leeftijd = 46;
+console.log(typeof leeftijd);
+leeftijd = "46";
+console.log(typeof(leeftijd));
