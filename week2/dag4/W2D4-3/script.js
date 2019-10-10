233b2afc48e56d393965db1815e7c657e63bf406
Doedels <sandermarkies@yahoo.com>
2019-10-10 15:41:16 +0200

    Toevoegen W2D4-opdracht 3

---
+++ week2/dag4/W2D4-3/script.js
@@ -0,0 +1,12 @@
+/* zet de functie om naar een arrow functie
+const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+  };
+  
+  ikRockArrowFunctions(); */
+
+const ikRockArrowFunctions = () => {
+    console.log("Joe, ik rock de arrow functions!");
+  };
+  
+  ikRockArrowFunctions();
